01ea8fcd3bc5bcb863b5c15c1eff501e9a95d762
DilipSundarraj <Dilip.Sundarraj@target.com>
2016-04-23 09:57:50 -0500

    BigDecimal Changes

--- CoreJava/src/main/java/com/learnCoreJava/BigDecimal/BigDecimalPractice.java
+++ CoreJava/src/main/java/com/learnCoreJava/BigDecimal/BigDecimalPractice.java
@@ -1,12 +1,20 @@
 package com.learnCoreJava.BigDecimal;
 
 import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 public class BigDecimalPractice {
 	
 	public static void main(String[] args) {
 		
-		BigDecimal bigDecimal=new BigDecimal("1.5667");
+		BigDecimal bigDecimal=new BigDecimal("1.573");
+		NumberFormat n = NumberFormat.getCurrencyInstance(Locale.US); 
+		NumberFormat n1 = NumberFormat.getCurrencyInstance(Locale.CANADA); 
+		String s = n.format(bigDecimal);
+		String s1 = n1.format(bigDecimal);
+		System.out.println("Cash value is  : "+ s);
+		System.out.println("Cash value is  : "+ s1);
 		BigDecimal bigDecimal1=new BigDecimal("1.5668");
 		
 		System.out.println(bigDecimal1.subtract(bigDecimal));
